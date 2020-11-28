96c54d3d3ebf8b709b20b3a9b591d0bf16d68102
basahota <techbasanta757@gmail.com>
2020-11-28 16:07:23 +0530

    lable changes

--- src/Second.js
+++ src/Second.js
@@ -10,13 +10,13 @@ export default class Second extends React.Component {
                     <b>{this.props.display_header}</b>
                 </h1>
                 <p>
-                    <label>EmployeeId : </label><b>{this.props.employee_id}</b>
+                    <label>EId : </label><b>{this.props.employee_id}</b>
                 </p>
                 <p>
-                    <label>EmployeeName : </label><b>{this.props.employee_name}</b>
+                    <label>EName : </label><b>{this.props.employee_name}</b>
                 </p>
                 <p>
-                    <label>EmployeeDept : </label><b>{this.props.employee_dept}</b>
+                    <label>Department : </label><b>{this.props.employee_dept}</b>
                 </p>
             </div>
         );
